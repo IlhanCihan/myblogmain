f883891519fcb1a38bf0fab87000c85d2808a11c
Cihan <cihanilhan.171@gmail.com>
2022-03-10 12:27:32 +0200

    first commxiaaaasaxt

--- components/HighlightCode.js
+++ components/HighlightCode.js
@@ -1,21 +1,21 @@
-
-
-import highlight from 'highlight.js';
-import { createRef, useEffect } from 'react';
-
-const HighlightCode = ({children, language}) => {
-  const code = createRef();
-
-  return (
-    <pre>
-      <code
-        ref={code}
-        className={language}>
-        {children}
-      </code>
-    </pre>
-  )
-}
-
-
-export default HighlightCode;
+//
+//
+// import highlight from 'highlight.js';
+// import { createRef, useEffect } from 'react';
+//
+// const HighlightCode = ({children, language}) => {
+//   const code = createRef();
+//
+//   return (
+//     <pre>
+//       <code
+//         ref={code}
+//         className={language}>
+//         {children}
+//       </code>
+//     </pre>
+//   )
+// }
+//
+//
+// export default HighlightCode;
